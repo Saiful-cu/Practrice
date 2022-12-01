98ba55b0525845e7a80fea37b843142f8bfae438
Saiful-cu <saiful.cu.19@gmail.com>
2022-12-01 18:17:41 +0600

    Print Pointer Array

--- 26.5 module/3rd_pointerArray.c
+++ 26.5 module/3rd_pointerArray.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
 
-void PrintArray(int arr[],int n);
+void PrintArray(int* arr,int n);
 
 int main()
 {
     int n;
     scanf("%d",&n);
+
     int arr[100];
+
 
     for(int i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&arr[i]); // user input of an array
     }
-    PrintArray(arr,n);
+
+    PrintArray(&arr,n);
 
     return 0;
 }
 
-void PrintArray(int arr[],int n)
+void PrintArray(int* arr,int n)
 {
     for(int i=0;i<n;i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d ",*arr+i);
     }
 }
