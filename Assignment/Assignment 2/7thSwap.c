c9044dcc0cc3aa791d936918633cafac83e6f23a
Saiful-cu <saiful.cu.19@gmail.com>
2022-12-02 10:22:31 +0600

    index value swap

--- Assignment/Assignment 2/7thSwap.c
+++ Assignment/Assignment 2/7thSwap.c
@@ -1,33 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>    // Include the standard input and output library.
 
-void swap(int* s, int* t)
-{
-    int temp;
-    temp = *s;
-    *s = *t;
-    *t = temp;
-}
+void swap(int *s, int *t);    // Declare swap function
 
 int main()
 {
-    int n,arr[1000];
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    int n, arr[1000];    // Declare variables n and arr with size of 1000
+    scanf("%d", &n);    // Take input for n
+    for (int i = 1; i <= n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);    // Take input for arr
     }
-    int numberOfTest,index1,index2;
-    scanf("%d",&numberOfTest);
+    int numberOfTest, index1, index2;    // Declare variable numberOfTest, index1 and index2
+    scanf("%d", &numberOfTest);    // Take input for numberOfTest
 
-    for(int i=0;i<numberOfTest;i++)
+    for (int i = 0; i < numberOfTest; i++)
     {
-        scanf("%d %d",&index1,&index2);
-        swap(&arr[index1],&arr[index2]);
+        scanf("%d %d", &index1, &index2);    // Take input for index1 and index2
+        swap(&arr[index1], &arr[index2]);    // Call swap function
     }
 
-    for(int i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d ", arr[i]);    // Print the value of arr
     }
     return 0;
+}
+
+void swap(int *s, int *t)
+{
+    int temp;    // Declare int variable temp
+    temp = *s;    // Assign value of s to temp
+    *s = *t;    // Assign value of t to s
+    *t = temp;    // Assign value of temp to t
 }
