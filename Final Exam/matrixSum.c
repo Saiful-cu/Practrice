e1b6a36eeeace41c2ec3492449789e13c754d205
Saiful-cu <saiful.cu.19@gmail.com>
2022-12-09 09:58:46 +0600

    Matrix Addition

--- Final Exam/matrixSum.c
+++ Final Exam/matrixSum.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
 
+int main()
+{
+    int mat[100][100],mat2[100][100];
+    int n,m;
+
+    scanf("%d %d",&n,&m);
+
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+           scanf("%d",&mat[i][j]);
+
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+           scanf("%d",&mat2[i][j]);
+
+        for(int i=0;i<n;i++)
+        {
+
+            for(int j=0;j<m;j++)
+        {
+            mat[i][j] =mat[i][j]+mat2[i][j];
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+        }
+
+
+    return 0;
+}
