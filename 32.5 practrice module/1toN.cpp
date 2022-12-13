72beb30df628438bd53359bc24b4b8e14c916e4f
Saiful-cu <saiful.cu.19@gmail.com>
2022-12-13 10:26:00 +0600

    Print 1 to N using Recursion

---
+++ 32.5 practrice module/1toN.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void print(int n)
+{
+    if(n>1)
+     print(n-1);
+    cout<<n<<" ";
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
